e1da9b7f565150f0a69cbede7d0e794a4390d363
wongselent <david.agung.satrio@gmail.com>
2018-11-26 21:36:44 +0800

    write metadata in "__init__.py" file

---
+++ __init__.py
@@ -0,0 +1,23 @@
+#!C:\Users\david.agung\AppData\Local\Continuum\miniconda2\envs\snaphunter\python.exe ./snaphunter/__init__.py
+
+""" 
+header file
+"""
+
+# define authorship information
+__authors__     = ['david Wong']
+__author__      = ','.join(__authors__)
+__credits__     = []
+__copyright__   = 'Copyright (c) 2018'
+__license__     = 'GPL'
+
+# maintanence information
+__maintainer__  = 'David Wong'
+__email__       = 'david.agung.satrio@gmail.com'
+
+# define version information
+__requires__        = ['PyQt4']
+__version_info__    = (0, 0, 0)
+__version__         = 'v%i.%02i.%02i' % __version_info__
+__revision__        = __version__
+
